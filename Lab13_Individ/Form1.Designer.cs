6ddb6bf70360f51d387f232daaac610412222e19
user <user@LAPTOP-VCVPT2J9>
2023-04-26 12:18:13 +0300

    Добавьте файлы проекта.

---
+++ Lab13_Individ/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Lab13_Individ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRazdel = new System.Windows.Forms.Button();
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRazdel
+            // 
+            this.btnRazdel.Location = new System.Drawing.Point(115, 355);
+            this.btnRazdel.Name = "btnRazdel";
+            this.btnRazdel.Size = new System.Drawing.Size(112, 23);
+            this.btnRazdel.TabIndex = 7;
+            this.btnRazdel.Text = "Раздел по RGB";
+            this.btnRazdel.UseVisualStyleBackColor = true;
+            this.btnRazdel.Click += new System.EventHandler(this.btnRazdel_Click);
+            // 
+            // pct
+            // 
+            this.pct.Location = new System.Drawing.Point(31, 12);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(280, 280);
+            this.pct.TabIndex = 6;
+            this.pct.TabStop = false;
+            this.pct.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pct_MouseDown);
+            this.pct.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pct_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(21, 355);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Открыть";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 387);
+            this.Controls.Add(this.btnRazdel);
+            this.Controls.Add(this.pct);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Индивидуальное задание, вариант 10";
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRazdel;
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ Lab13_Individ/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab13_Individ
+{
+    public partial class Form1 : Form
+    {
+        // Глобальные переменные
+        private Point PreviousPoint, point; private Bitmap bmp;
+        private Pen blackPen; private Graphics g;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            // Описываем объект класса OpenFileDialog
+            OpenFileDialog dialog = new OpenFileDialog();
+            // Задаем расширения файлов
+            dialog.Filter = "Image files (*.BMP, *.JPG, " + "*.GIF, *.PNG)|*.bmp;*.jpg;*.gif;*.png";
+            // Вызываем диалог и проверяем выбран ли файл
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // Загружаем изображение из выбранного файла
+                System.Drawing.Image image = System.Drawing.Image.FromFile(dialog.FileName);
+
+                // Создаем и загружаем изображение в формате
+                bmp = new Bitmap(image, pct.Width, pct.Height);
+                // Записываем изображение в pictureBox1
+                pct.Image = bmp;
+                // Подготавливаем объект Graphics для рисования
+                g = Graphics.FromImage(pct.Image);
+            }
+        }
+        private void btnRazdel_Click(object sender, EventArgs e)
+        {
+            int leftTopWid = pct.Width / 2;
+            int leftTopHeig = pct.Height / 2;
+
+
+            for (int i = 0; i < leftTopWid; i++) //Покраска в красный
+                for (int j = 0; j < leftTopHeig; j++)
+                {
+                    int R = bmp.GetPixel(i, j).R;
+
+                    Color p = Color.FromArgb(255, R, 0, 0);
+                    bmp.SetPixel(i, j, p);
+                }
+
+            for (int i = leftTopWid; i < pct.Width; i++) //Покраска в зеленый
+            {
+                for (int j = 0; j < leftTopHeig; j++)
+                {
+                    int G = bmp.GetPixel(i, j).G;
+                    Color p = Color.FromArgb(255, 0, G, 0);
+
+                    bmp.SetPixel(i, j, p);
+                }
+            }
+
+            for (int i = 0; i < leftTopWid; i++) //Покраска в синий
+            {
+                for (int j = leftTopHeig; j < pct.Height; j++)
+                {
+                    int B = bmp.GetPixel(i, j).B;
+                    Color p = Color.FromArgb(255, 0, 0, B);
+
+                    bmp.SetPixel(i, j, p);
+                }
+            }
+
+            for (int i = leftTopHeig; i < pct.Width; i++) //Покраска в серый
+            {
+                for (int j = leftTopHeig; j < pct.Height; j++)
+                {
+                    // Извлекаем в R значение красного цвета
+                    int R = bmp.GetPixel(i, j).R;
+                    // Извлекаем в G значение зеленого цвета
+                    int G = bmp.GetPixel(i, j).G;
+                    // Извлекаем в B значение синего цвета
+                    int B = bmp.GetPixel(i, j).B;
+                    // Высчитываем среднее арифметическое
+                    int Gray = (R + G + B) / 3;
+
+                    Color p = Color.FromArgb(255, Gray, Gray, Gray);
+
+                    bmp.SetPixel(i, j, p);
+                }
+            }
+            // Вызываем функцию перерисовки окна
+            Refresh();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Описываем и порождаем объект savedialog
+            SaveFileDialog savedialog = new SaveFileDialog();
+            // Задаем свойства для savedialog
+            savedialog.Title = "Сохранить картинку как ...";
+            savedialog.OverwritePrompt = true;
+            savedialog.CheckPathExists = true;
+            savedialog.Filter = "Bitmap File(*.bmp)|*.bmp|" + "GIF File(*.gif)|*.gif|" + "JPEG File(*.jpg)|*.jpg|" + "PNG File(*.png)|*.png";
+            // Показываем диалог и проверяем задано ли имя файла
+            if (savedialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = savedialog.FileName;
+                // Убираем из имени расширение файла
+                string strFilExtn = fileName.Remove(0, fileName.Length - 3);
+                // Сохраняем файл в нужном формате
+                switch (strFilExtn)
+                {
+                    case "bmp":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp); break;
+                    case "jpg":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "gif":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif); break;
+                    case "tif":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                    case "png":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png); break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void pct_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Записываем в предыдущую точку текущие координаты
+            PreviousPoint.X = e.X; PreviousPoint.Y = e.Y;
+        }
+
+        private void pct_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Проверяем нажата ли левая кнопка мыши
+            if (e.Button == MouseButtons.Left)
+            {
+                // Запоминаем текущее положение курсора мыши
+                point.X = e.X; point.Y = e.Y;
+                // Соеденяем линией предыдущую точку с текущей
+                g.DrawLine(blackPen, PreviousPoint, point);
+                // Текущее положение курсора ‐ в PreviousPoint
+                PreviousPoint.X = point.X;
+                PreviousPoint.Y = point.Y;
+                // Принудительно вызываем перерисовку
+                pct.Invalidate();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            blackPen = new Pen(Color.Black, 4);
+        }
+    }
+}
